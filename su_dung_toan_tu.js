f60e6b954ee52b1739134173a043a4794ea44375
vipllaanhki16 <vipllaanhki16@gmail.com>
2019-02-25 20:20:57 +0700

    Bien,kieu du lieu va toan tu

---
+++ su_dung_toan_tu.js
@@ -0,0 +1,6 @@
+var width;
+var height;
+var width=parseInt(prompt("Enter the width: "));
+var height=parseInt(prompt("Enter the height: "));
+var area = width*height;
+document.write("The area is: "+area);
